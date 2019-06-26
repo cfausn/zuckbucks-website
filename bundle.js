3d23a14db9be55630f4b8a3f6cb98e80fcff9c12
cfausn <cfausn@users.noreply.github.com>
2019-06-25 20:11:46 -0400

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -35,7 +35,7 @@ window.onload = function(){
     document.getElementById("total").innerHTML = numberWithCommas(parseInt(data.result, 16))
   });
 
-  var circulatingUrl = "https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x7090a6e22c838469c9e67851d6489ba9c933a43f&address=0x5ff87907d6157f18732ce912153149a3f9362a0b&tag=latest&apikey=W1ICGCDZ4TQBKRM5QKBRSCR3WXG4NZIASK";
+  var circulatingUrl = "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x7090a6e22c838469c9e67851d6489ba9c933a43f&data=0x9358928b&tag=latest&apikey=W1ICGCDZ4TQBKRM5QKBRSCR3WXG4NZIASK";
   $.ajax(circulatingUrl, {
       cache: false,
       dataType: "json"

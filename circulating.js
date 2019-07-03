120018761fe5d9b619b01ad7bb5775bfce0bcba6
Colin Fausnaught <cfausn@gmail.com>
2019-07-03 10:43:55 -0400

    changed JS

---
+++ circulating.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  var circulatingUrl = "https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x7090a6e22c838469c9e67851d6489ba9c933a43f&address=0x5ff87907d6157f18732ce912153149a3f9362a0b&tag=latest&apikey=W1ICGCDZ4TQBKRM5QKBRSCR3WXG4NZIASK"
+  $.ajax(circulatingUrl, {
+      cache: false,
+      dataType: "json"
+  }).then(function (data){
+    document.getElementById("circulating").innerHTML = numberWithCommas(1500000 - parseInt(data.result));
+  });
+}
